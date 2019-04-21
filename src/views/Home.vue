c71efa3226db6853cde866d55b2f56a40b864d89
serhatyuna <serhatyuna@gmail.com>
2019-04-21 23:13:04 +0300

    fetch data from the API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,47 @@
 <template>
-  <main class="home"></main>
+  <main class="home">
+    <div class="story" v-for="story in stories" :key="story">
+      <h2>{{ story.data.title }}</h2>
+      <p>{{ story.data.url }}</p>
+      <p>Comments: {{ story.data.descendants }}</p>
+      <p>Score: {{ story.data.score }}</p>
+      {{ story }}
+    </div>
+  </main>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'home'
+  name: 'home',
+  data: function() {
+    return {
+      err: '',
+      stories: []
+    }
+  },
+  created: async function() {
+    // https://hacker-news.firebaseio.com/v0/topstories.json
+    axios
+      .get('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then(response => {
+        let limitedResponse = response.data.slice(0, 15)
+        limitedResponse.forEach(id => {
+          axios
+            .get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json')
+            .then(response => {
+              this.stories.push(response)
+            })
+            .catch(err => {
+              this.err = err
+            })
+        })
+      })
+      .catch(err => {
+        this.err = err
+      })
+  }
 }
 </script>
 
